35994b6ff504ecf40d98c9bee17cbaf6db63e9cc
NeirAzer <rezkyazharsuryaputra@gmail.com>
2026-02-05 10:44:28 +0700

    Final

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        account: 'account.html',
+        wishlist: 'wishlist.html',
+        checkout: 'checkout.html',
+        register: 'register.html',
+        view: 'view.html',
+        login: 'login.html',
+        shop: 'shop.html'
+      }
+    }
+  },
   plugins: [
     tailwindcss(),
   ],
